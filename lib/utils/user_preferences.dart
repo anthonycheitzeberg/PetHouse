5a50c0de6af2282211c3f9168df50d0eb0812e40
anthonycheitzeberg <a.heitzeberg007@gmail.com>
2021-09-20 10:11:07 +0800

    add assets, fix file structure, make profile page

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,11 @@
+import 'package:pet_house/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath: '',
+    name: 'Anthony Heitzeberg',
+    email: 'a.heitzeberg007@gmail.com',
+    about: 'Simple dude.',
+    isDarkMode: false,
+  );
+}
